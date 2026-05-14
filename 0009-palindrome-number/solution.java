f86ab10e73c527011db8553e495f5813f44f6f9d
HemaNamrathaChavala <109197997+HemaNamrathaChavala@users.noreply.github.com>
2026-05-14 12:28:23 +0000

    Sync LeetCode submission Runtime - 5 ms (82.41%), Memory - 45.8 MB (86.70%)

--- 0009-palindrome-number/solution.java
+++ 0009-palindrome-number/solution.java
@@ -1,27 +1,18 @@
-import java.util.*;
 class Solution {
     public boolean isPalindrome(int x) {
-        int sum=0,r=0;
-        int n=x;
-        if(x<0)
-            return false;
-        while(x>0)
-        {
-            r=x%10;
-            sum=(sum*10)+r;
-            x=x/10;
+       
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
         }
-        if(sum==n)
-            return true;
-        else
-            return false;
-        
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        Solution s=new Solution();
-        System.out.println(s.isPalindrome(x));
+
+        return original == reversed;
+
     }
 }
