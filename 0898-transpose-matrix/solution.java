06404a8ab42ad08ff4239b8942ba8944a7db309f
HemaNamrathaChavala <109197997+HemaNamrathaChavala@users.noreply.github.com>
2026-05-21 12:27:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.8 MB (48.46%)

---
+++ 0898-transpose-matrix/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transpose = new int[columns][row];
+        for(int i=0; i<row; i++){
+            for(int j=0; j< columns; j++){
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+
+    }
+}
